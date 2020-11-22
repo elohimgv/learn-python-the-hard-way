3b61a9847ff9e26465d3628c385318d7f396800e
elohim <elohim.guevara@gmail.com>
2020-11-22 12:55:13 -0600

    add: ex3.py
    Comments and more comments on code and doing some math operations and boolean operations.

---
+++ mystuff/ex/ex3.py
@@ -0,0 +1,28 @@
+# Print on console: ...will now count my chicknes:
+print("I will now count my chicknes:")
+# Print the number of hens
+print("Hens", 25 + 30 / 6)
+# Print the number of roosters
+print("Roosters", 100 - 25 * 3 % 4)
+# Print on console ...will count the eggs
+print("Now I will count the eggs:")
+# doing a operation
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# Question: is it true that 3 + 2 < - 7?
+print("Is it true that 3 + 2 < 5 - 7?")
+# doing boolean operation after math operation
+print(3 + 2 < 5 - 7)
+# Question: what is 3 + 2? and then answer: math operation
+print("What is 3 + 2?", 3 + 2)
+# Question: what is 5 - 7? and then answer: math operation
+print("What is 5 - 7?", 5 - 7)
+# Giving an explanation why is False
+print("Oh, that's why it's False:")
+# more explanation...
+print("How about some more.")
+# boolean operation: 5 > -2
+print("Is it greater?", 5 > -2)
+# boolean operation: 5 >= -2
+print("Is it greater or equal?", 5 >= -2)
+# boolean operation: 5 <= -2
+print("Is it less or eaqual", 5 <= -2)
