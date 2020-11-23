ebe374c560cf5325286c2d32713e6a9590b5c6dc
elohim <elohim.guevara@gmail.com>
2020-11-23 16:16:43 -0600

    add: ex10.py
    Escape sequences and format string

---
+++ mystuff/ex/ex10.py
@@ -0,0 +1,23 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+'''
+
+panther = "\v\ngrrrr\v\ngrrr\v\ngrr"
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print("%s" % panther)
+print('.' * 20)
+print('.\n' * 5)
+print('.\f' * 20)
+
+
