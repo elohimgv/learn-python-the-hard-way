124e63a849006bbb165220038bb33353d644b75a
elohim <elohim.guevara@gmail.com>
2020-11-23 16:42:54 -0600

    add: ex11.py
    Asking questions.

---
+++ mystuff/ex/ex11.py
@@ -0,0 +1,12 @@
+print("How old are you?")
+age = input()
+# age = input("How old are you?")
+print("How tall are you?")
+height = input()
+# height = input("How tall are you?")
+print("How much do you weight?")
+weight = input()
+# weight = input("How much do you weight?")
+
+print("So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight))
